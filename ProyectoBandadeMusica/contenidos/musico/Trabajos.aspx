8dceaa8fbb8c9ee5c7ce6f2ffab9cb6e0831da37
xelphil <manuelgarin2@hotmail.com>
2018-06-08 11:06:04 +0200

    Añadida la pagina para gestionar trabajos del musico y preparación para la unión a un trabajo

---
+++ ProyectoBandadeMusica/contenidos/musico/Trabajos.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Trabajos.aspx.vb" Inherits="contenidos_musico_Trabajos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <div>
+            <asp:Image ID="imgLogo" runat="server" ImageUrl="~/imagenes/emblemmatic-bandademusica-logo-10.png"/>
+            <asp:Button ID="btCerrarSesion" runat="server" Text="Cerrar Sesion" />
+        </div> 
+       <nav>
+           <asp:Button ID="btTrabajos" runat="server" Text="Trabajos" />
+           <asp:Button ID="btMusicos" runat="server" Text="Musicos" />
+           <asp:Button ID="btGastos" runat="server" Text="Gastos" />
+       </nav>
+        <section>
+            <h1>Trabajos</h1>
+     <asp:Panel ID="pnBuscador" runat="server">
+            <asp:Panel ID="pnBuscar" runat="server">
+                <asp:Label ID="lbBuscar" runat="server" Text="Buscar por: "></asp:Label>
+                <asp:DropDownList ID="ddlBuscar" runat="server">
+                    <asp:ListItem Text="Nombre" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="Ciudad" Value="2"></asp:ListItem>
+                    <asp:ListItem Text="Completo" Value="3"></asp:ListItem>
+                </asp:DropDownList>
+                <asp:TextBox ID="tbBuscar" runat="server"></asp:TextBox>
+                <asp:Button ID="btBusqueda" runat="server" Text="Buscar..." />
+            </asp:Panel>
+            <asp:GridView ID="gvMusicos" runat="server" AutoGenerateColumns="False" DataKeyNames="id">
+                <Columns>
+                    <asp:CommandField ShowSelectButton="True" ButtonType="Button" />
+                    <asp:BoundField DataField="id" HeaderText="id" ReadOnly="True" InsertVisible="False" SortExpression="id" Visible="false"/>
+                    <asp:BoundField DataField="nombre" HeaderText="Nombre del Trabajo" SortExpression="nombre" />
+                    <asp:BoundField DataField="fecha" HeaderText="Fecha del Trabajo" SortExpression="fecha" />
+                    <asp:BoundField DataField="lugar" HeaderText="Lugar del Trabajo" SortExpression="lugar" />
+                    <asp:BoundField DataField="ciudad" HeaderText="Ciudad donde se realiza el trabajo(Convenio)" SortExpression="ciudad" />
+                    <asp:BoundField DataField="direccion" HeaderText="Direccion del Trabajo" SortExpression="direccion" />
+                    <asp:BoundField DataField="descripcion" HeaderText="Descripcion del Trabajo" SortExpression="descripcion" />
+                    <asp:BoundField DataField="categoriatrabajo" HeaderText="Categoria del Trabajo" SortExpression="categoriatrabajo"/>
+                    <asp:CheckBoxField DataField="completo" HeaderText="¿Esta completo el trabajo?" SortExpression="completo" />
+                </Columns>
+            </asp:GridView>
+        </asp:Panel>
+            <asp:Button ID="btMusicoTrabajos" runat="server" Text="Buscar trabajos disponibles" />
+        <asp:Button ID="btModificar" runat="server" Text="Modificar Músico" />
+        <asp:Button ID="btBorrar" runat="server" Text="Borrar Músico" />
+    <br />
+   <asp:Panel ID="pnDatos" runat="server">
+    <asp:Label ID="lbNombre" runat="server" Text="Nombre: "></asp:Label>
+    <asp:TextBox ID="tbNombre" runat="server"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="rfvNombre" runat="server" ErrorMessage="Se requiere introducir un nombre al músico" ControlToValidate="tbNombre" ValidationGroup="DatosMusico"></asp:RequiredFieldValidator>
+    <br />
+       <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <asp:Label ID="lbFecha" runat="server" Text="Fecha: "></asp:Label>
+                <asp:Calendar ID="clFecha" runat="server" BackColor="White" BorderColor="#999999" CellPadding="4" DayNameFormat="Shortest" Font-Names="Verdana" Font-Size="8pt" ForeColor="Black" Height="180px" Width="200px">
+                    <DayHeaderStyle BackColor="#CCCCCC" Font-Bold="True" Font-Size="7pt" />
+                    <NextPrevStyle VerticalAlign="Bottom" />
+                    <OtherMonthDayStyle ForeColor="#808080" />
+                    <SelectedDayStyle BackColor="#666666" Font-Bold="True" ForeColor="White" />
+                    <SelectorStyle BackColor="#CCCCCC" />
+                    <TitleStyle BackColor="#999999" BorderColor="Black" Font-Bold="True" />
+                    <TodayDayStyle BackColor="#CCCCCC" ForeColor="Black" />
+                    <WeekendDayStyle BackColor="#FFFFCC" />
+                   </asp:Calendar>
+                <asp:RequiredFieldValidator ID="rfvFecha" runat="server" ErrorMessage="Se requiere introducir una fecha del gasto" ControlToValidate="tbNombre" ValidationGroup="DatosMusico"></asp:RequiredFieldValidator>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+       <asp:Label ID="lbLugar" runat="server" Text="Lugar: "></asp:Label>
+    <asp:TextBox ID="tbLugar" runat="server"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="rfvLugar" runat="server" ErrorMessage="Se requiere introducir el lugar del trabajo" ControlToValidate="tbLugar" ValidationGroup="DatosMusico"></asp:RequiredFieldValidator>
+    <br />
+       <asp:Label ID="lbCiudad" runat="server" Text="Ciudad: "></asp:Label>
+    <asp:TextBox ID="tbCiudad" runat="server"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="rfvCiudad" runat="server" ErrorMessage="Se requiere introducir la ciudad al músico" ControlToValidate="tbCiudad" ValidationGroup="DatosMusico"></asp:RequiredFieldValidator>
+    <br />
+    <asp:Label ID="lbDireccion" runat="server" Text="Direccion: "></asp:Label>
+    <asp:TextBox ID="tbDireccion" runat="server"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="rfvDireccion" runat="server" ErrorMessage="Se requiere introducir la dirección al músico" ControlToValidate="tbDireccion" ValidationGroup="DatosMusico"></asp:RequiredFieldValidator>
+    <br />
+    <asp:Label ID="lbDescripcion" runat="server" Text="Descripción: "></asp:Label>
+    <asp:TextBox ID="tbDescripcion" runat="server"></asp:TextBox>
+    <br />
+       <asp:Label ID="lbCategoriatrabajo" runat="server" Text="Categoria de Trabajo: "></asp:Label>
+    <asp:DropDownList ID="ddlcategoriatrabajo" runat="server" DataSourceID="SqlDCategoriaTrabajo" DataTextField="nombre" DataValueField="id"></asp:DropDownList>
+    <asp:SqlDataSource ID="SqlDCategoriaTrabajo" runat="server" ConnectionString="<%$ ConnectionStrings:BandaDeMusicaConnectionString %>" SelectCommand="SELECT [id], [nombre] FROM [CategoriaTrabajo]"></asp:SqlDataSource>
+    <br />
+    <asp:Label ID="lbCompleto" runat="server" Text="¿Trabajo Completo?: "></asp:Label>
+    <asp:CheckBox ID="cbCompleto" runat="server" />
+       <br />
+    <br />
+    <asp:Button ID="btGuardar" runat="server" Text="Guardar" ValidationGroup="DatosMusico"/>
+    <asp:Button ID="btCancelar" runat="server" Text="Cancelar" />
+   </asp:Panel>
+        </section>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ProyectoBandadeMusica/contenidos/musico/Trabajos.aspx.vb
@@ -0,0 +1,202 @@
+﻿
+Imports System.Data
+Imports System.Data.SqlClient
+
+Partial Class contenidos_musico_Trabajos
+    Inherits System.Web.UI.Page
+    Dim cadena As String = "Data Source=(local);Initial Catalog=BandaDeMusica;Integrated Security=SSPI;"
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        pnDatos.Enabled = False
+        listarMusicos()
+        btBorrar.Enabled = False
+    End Sub
+
+    Protected Sub btGuardar_Click(sender As Object, e As EventArgs) Handles btGuardar.Click
+        MsgBox(Session("modifica").ToString)
+        If Session("modifica").ToString = False Then
+            Dim cnxInsert As New SqlConnection(cadena)
+            Dim sentenciaInsert As String = "insert into Trabajo(nombre,fecha,lugar,ciudad,direccion,descripcion,categoriatrabajo,completo) values(@nombre,@fecha,@lugar,@ciudad,@direccion,@descripcion,@categoriatrabajo,@completo)"
+            Dim cmdInsert As New SqlCommand(sentenciaInsert, cnxInsert)
+            cmdInsert.Parameters.AddWithValue("@nombre", tbNombre.Text)
+            cmdInsert.Parameters.AddWithValue("@fecha", clFecha.SelectedDate)
+            cmdInsert.Parameters.AddWithValue("@lugar", tbLugar.Text)
+            cmdInsert.Parameters.AddWithValue("@ciudad", tbCiudad.Text)
+            cmdInsert.Parameters.AddWithValue("@direccion", tbDireccion.Text)
+            cmdInsert.Parameters.AddWithValue("@descripcion", tbDescripcion.Text)
+            cmdInsert.Parameters.AddWithValue("@categoriatrabajo", ddlcategoriatrabajo.SelectedValue)
+            cmdInsert.Parameters.AddWithValue("@completo", cbCompleto.Checked)
+            Try
+                cnxInsert.Open()
+                If cnxInsert.State = Data.ConnectionState.Open Then
+                    cmdInsert.ExecuteNonQuery()
+                    cmdInsert.Parameters.Clear()
+                    LimpiarCampos()
+                End If
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            Finally
+                cnxInsert.Close()
+                cnxInsert.Dispose()
+
+                Response.Redirect("~/Contenidos/musico/Trabajos.aspx")
+            End Try
+        Else
+            MsgBox(gvMusicos.SelectedDataKey.Value.ToString)
+            Dim cnxInsert As New SqlConnection(cadena)
+            Dim sentenciaInsert As String = "update Trabajo set nombre=@nombre,fecha=@fecha,lugar=@lugar,ciudad=@ciudad,direccion=@direccion,descripcion=@descripcion,categoriatrabajo=@categoriatrabajo,completo=@completo where id=@id"
+            Dim cmdInsert As New SqlCommand(sentenciaInsert, cnxInsert)
+            cmdInsert.Parameters.AddWithValue("@id", gvMusicos.SelectedDataKey.Value.ToString)
+            cmdInsert.Parameters.AddWithValue("@nombre", tbNombre.Text)
+            cmdInsert.Parameters.AddWithValue("@fecha", clFecha.SelectedDate)
+            cmdInsert.Parameters.AddWithValue("@lugar", tbLugar.Text)
+            cmdInsert.Parameters.AddWithValue("@ciudad", tbCiudad.Text)
+            cmdInsert.Parameters.AddWithValue("@direccion", tbDireccion.Text)
+            cmdInsert.Parameters.AddWithValue("@descripcion", tbDescripcion.Text)
+            cmdInsert.Parameters.AddWithValue("@categoriatrabajo", ddlcategoriatrabajo.SelectedValue)
+            cmdInsert.Parameters.AddWithValue("@completo", cbCompleto.Checked)
+            Try
+                cnxInsert.Open()
+                If cnxInsert.State = Data.ConnectionState.Open Then
+                    cmdInsert.ExecuteNonQuery()
+                    cmdInsert.Parameters.Clear()
+
+                End If
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            Finally
+                cnxInsert.Close()
+                cnxInsert.Dispose()
+                Response.Redirect("~/Contenidos/musico/Trabajos.aspx")
+            End Try
+        End If
+    End Sub
+    Protected Sub LimpiarCampos()
+        tbNombre.Text = ""
+        tbLugar.Text = ""
+        tbCiudad.Text = ""
+        tbDireccion.Text = ""
+        tbDescripcion.Text = ""
+
+        clFecha.SelectedDate = clFecha.TodaysDate
+
+        cbCompleto.Checked = False
+
+        ddlcategoriatrabajo.SelectedIndex = -1
+    End Sub
+    Public Sub listarMusicos()
+        ''cambiar a los usuarios
+        Dim sentenciaBuscar As String = "select Trabajo.id,Trabajo.nombre,Trabajo.fecha,Trabajo.lugar,Trabajo.ciudad,Trabajo.direccion,Trabajo.descripcion,Trabajo.categoriatrabajo,Trabajo.completo from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo where MusicoTrabajos.idMusicos=@idMusicos"
+        Dim cnxBuscar As New SqlConnection(cadena)
+        Dim cmdBuscar As New SqlCommand(sentenciaBuscar, cnxBuscar)
+        cmdBuscar.Parameters.AddWithValue("@idMusicos", Session("dUsuario").Rows(0).item(0))
+        Dim adaptadorBuscar As New SqlDataAdapter(cmdBuscar)
+        Dim dt As New DataTable
+        adaptadorBuscar.Fill(dt)
+        gvMusicos.DataSource = dt
+        gvMusicos.DataBind()
+    End Sub
+    Protected Sub btCancelar_Click(sender As Object, e As EventArgs) Handles btCancelar.Click
+        LimpiarCampos()
+        btBorrar.Enabled = False
+        btModificar.Enabled = False
+    End Sub
+
+    Protected Sub gvMusicos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles gvMusicos.SelectedIndexChanged
+        'MsgBox(gvMusicos.SelectedDataKey.Value.ToString)
+        btModificar.Enabled = True
+        btBorrar.Enabled = True
+
+        Dim idCliente = gvMusicos.SelectedDataKey.Value
+        Dim sentenciaCliente As String = "select * from Trabajo where id=@id"
+        Dim cnxCliente As New SqlConnection(cadena)
+        Dim cmdCliente As New SqlCommand(sentenciaCliente, cnxCliente)
+        cmdCliente.Parameters.AddWithValue("@id", gvMusicos.SelectedDataKey.Value)
+        Dim adaptadorCliente As New SqlDataAdapter(cmdCliente)
+        Dim dt As New DataTable
+        adaptadorCliente.Fill(dt)
+        Dim fila As DataRow = dt.Rows(0)
+
+        tbNombre.Text = fila("nombre").ToString
+        clFecha.SelectedDate = fila("fecha").ToString
+        tbLugar.Text = fila("lugar").ToString
+        tbCiudad.Text = fila("ciudad").ToString
+        tbDireccion.Text = fila("direccion").ToString
+        tbDescripcion.Text = fila("descripcion").ToString
+        cbCompleto.Checked = fila("completo")
+        ddlcategoriatrabajo.SelectedIndex = -1
+        ddlcategoriatrabajo.SelectedValue = fila("categoriatrabajo")
+
+    End Sub
+
+    Protected Sub btBusqueda_Click(sender As Object, e As EventArgs) Handles btBusqueda.Click
+        Dim sentenciaBuscar As String = "select * from Trabajo"
+        If Not tbBuscar.Text = Nothing Then
+            If ddlBuscar.SelectedValue = 1 Then
+                sentenciaBuscar = "select Trabajo.id,Trabajo.nombre,Trabajo.fecha,Trabajo.lugar,Trabajo.ciudad,Trabajo.direccion,Trabajo.descripcion,Trabajo.categoriatrabajo,Trabajo.completo from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo where MusicoTrabajos.idMusicos=@idMusicos and nombre like '%" & tbBuscar.Text & "%'"
+            End If
+            If ddlBuscar.SelectedValue = 2 Then
+                sentenciaBuscar = "select Trabajo.id,Trabajo.nombre,Trabajo.fecha,Trabajo.lugar,Trabajo.ciudad,Trabajo.direccion,Trabajo.descripcion,Trabajo.categoriatrabajo,Trabajo.completo from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo where MusicoTrabajos.idMusicos=@idMusicos and ciudad like '%" & tbBuscar.Text & "%'"
+            End If
+            If ddlBuscar.SelectedValue = 3 Then
+                sentenciaBuscar = "select Trabajo.id,Trabajo.nombre,Trabajo.fecha,Trabajo.lugar,Trabajo.ciudad,Trabajo.direccion,Trabajo.descripcion,Trabajo.categoriatrabajo,Trabajo.completo from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo where MusicoTrabajos.idMusicos=@idMusicos and completo = " & tbBuscar.Text & ""
+            End If
+            Dim cnxBuscar As New SqlConnection(cadena)
+            Dim cmdBuscar As New SqlCommand(sentenciaBuscar, cnxBuscar)
+            cmdBuscar.Parameters.AddWithValue("@idMusicos", Session("dUsuario").Rows(0).item(0))
+            Dim adaptadorBuscar As New SqlDataAdapter(cmdBuscar)
+            Dim dt As New DataTable
+            adaptadorBuscar.Fill(dt)
+            gvMusicos.DataSource = dt
+            gvMusicos.DataBind()
+            LimpiarCampos()
+        End If
+
+    End Sub
+
+    Protected Sub btBorrar_Click(sender As Object, e As EventArgs) Handles btBorrar.Click
+        Dim cnxInsert As New SqlConnection(cadena)
+        Dim sentenciaInsert As String = "delete from Musicos where id=@id"
+        Dim cmdInsert As New SqlCommand(sentenciaInsert, cnxInsert)
+        cmdInsert.Parameters.AddWithValue("@id", gvMusicos.SelectedDataKey.Value)
+        Try
+            cnxInsert.Open()
+            If cnxInsert.State = Data.ConnectionState.Open Then
+                cmdInsert.ExecuteNonQuery()
+                cmdInsert.Parameters.Clear()
+
+            End If
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+        Finally
+            cnxInsert.Close()
+            cnxInsert.Dispose()
+            LimpiarCampos()
+            Response.Redirect("~/Contenidos/musico/Trabajos.aspx")
+        End Try
+    End Sub
+
+    Protected Sub btModificar_Click(sender As Object, e As EventArgs) Handles btModificar.Click
+        Session("modifica") = True
+        pnDatos.Enabled = True
+        MsgBox(Session("modifica").ToString)
+    End Sub
+    Protected Sub btMusicoTrabajos_Click(sender As Object, e As EventArgs) Handles btMusicoTrabajos.Click
+        Response.Redirect("~/Contenidos/musico/MusicoTrabajos.aspx")
+    End Sub
+
+    Protected Sub btMusicos_Click(sender As Object, e As EventArgs) Handles btMusicos.Click
+        Response.Redirect("~/Contenidos/musico/Musicos.aspx")
+    End Sub
+
+    Protected Sub btCerrarSesion_Click(sender As Object, e As EventArgs) Handles btCerrarSesion.Click
+        codigo.salir(Context.User.Identity.Name)
+    End Sub
+
+    Protected Sub btTrabajos_Click(sender As Object, e As EventArgs) Handles btTrabajos.Click
+        Response.Redirect("~/Contenidos/musico/Trabajos.aspx")
+    End Sub
+
+    Protected Sub btGastos_Click(sender As Object, e As EventArgs) Handles btGastos.Click
+        Response.Redirect("~/Contenidos/musico/Gastos.aspx")
+    End Sub
+End Class
