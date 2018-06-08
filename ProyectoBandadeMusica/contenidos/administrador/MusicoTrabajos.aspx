5e60a91e1c0ed15cb44835f26c7a2967e1fbc3ea
xelphil <manuelgarin2@hotmail.com>
2018-06-08 11:08:53 +0200

    Añadida la pagina para añadir un musico a un trabajo

---
+++ ProyectoBandadeMusica/contenidos/administrador/MusicoTrabajos.aspx
@@ -0,0 +1,71 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="MusicoTrabajos.aspx.vb" Inherits="contenidos_administrador_MusicoTrabajos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <div>
+            <asp:Image ID="imgLogo" runat="server" ImageUrl="~/imagenes/emblemmatic-bandademusica-logo-10.png"/>
+            <asp:Button ID="btCerrarSesion" runat="server" Text="Cerrar Sesion" />
+        </div> 
+       <nav>
+           <asp:Button ID="btTrabajos" runat="server" Text="Trabajos" />
+           <asp:Button ID="btMusicos" runat="server" Text="Musicos" />
+           <asp:Button ID="btGastos" runat="server" Text="Gastos" />
+           <asp:Button ID="btCatTrab" runat="server" Text="Categoría Trabajos" />
+           <asp:Button ID="btCatMus" runat="server" Text="Categoría Musicos" />
+           <asp:Button ID="btInstrumentos" runat="server" Text="Instrumentos" />
+           <asp:Button ID="btUsuarios" runat="server" Text="Usuarios" />
+       </nav>
+        <section>
+            <h1>Trabajos</h1>
+     <asp:Panel ID="pnBuscador" runat="server">
+            <asp:Panel ID="pnBuscar" runat="server">
+                <asp:Label ID="lbBuscar" runat="server" Text="Buscar por: "></asp:Label>
+                <asp:DropDownList ID="ddlBuscar" runat="server">
+                    <asp:ListItem Text="Nombre Trabajo" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="Nombre Músico" Value="2"></asp:ListItem>
+                </asp:DropDownList>
+                <asp:TextBox ID="tbBuscar" runat="server"></asp:TextBox>
+                <asp:Button ID="btBusqueda" runat="server" Text="Buscar..." />
+            </asp:Panel>
+            <asp:GridView ID="gvMusicos" runat="server" AutoGenerateColumns="False" DataKeyNames="idTrabajo">
+                <Columns>
+                    <asp:CommandField ShowSelectButton="True" ButtonType="Button" />
+                    <asp:BoundField DataField="nombre" HeaderText="Nombre del Trabajo" SortExpression="nombre"/>
+                    <asp:BoundField DataField="descripcion" HeaderText="Descripcion del Trabajo" SortExpression="descripcion" />
+                    <asp:BoundField DataField="fecha" HeaderText="Fecha del Trabajo" SortExpression="fecha" />
+                    <asp:BoundField DataField="idTrabajo" HeaderText="idTrabajo" SortExpression="idTrabajo" ReadOnly="True" />
+                    <asp:BoundField DataField="nombre1" HeaderText="Nombre del Musico" SortExpression="nombre1" />
+                    <asp:BoundField DataField="apellidos" HeaderText="Apellidos del Musico" SortExpression="apellidos" />
+                    <asp:BoundField DataField="idMusicos" HeaderText="idMusicos" SortExpression="idMusicos" ReadOnly="True" />
+                </Columns>
+            </asp:GridView>
+        </asp:Panel>
+         <asp:Button ID="btMusicoTrabajos" runat="server" Text="Volver a trabajos" />
+         <asp:Button ID="btCrear" runat="server" Text="Crear Músico" />
+        <asp:Button ID="btBorrar" runat="server" Text="Borrar Músico" />
+    <br />
+   <asp:Panel ID="pnDatos" runat="server">
+
+    <asp:Label ID="lbTrabajo" runat="server" Text="Trabajo: "></asp:Label>
+    <asp:DropDownList ID="ddltrabajo" runat="server" DataSourceID="SqlDCategoriaTrabajo" DataTextField="nombre" DataValueField="id"></asp:DropDownList>
+    <asp:SqlDataSource ID="SqlDCategoriaTrabajo" runat="server" ConnectionString="<%$ ConnectionStrings:BandaDeMusicaConnectionString %>" SelectCommand="SELECT [nombre], [id] FROM [Trabajo]"></asp:SqlDataSource>
+    <br />
+    <asp:Label ID="lbMusico" runat="server" Text="Musico: "></asp:Label>
+    <asp:DropDownList ID="ddlMusico" runat="server" DataSourceID="SqlDataSource1" DataTextField="nombre" DataValueField="idMusico" ></asp:DropDownList>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:BandaDeMusicaConnectionString %>" SelectCommand="SELECT [idMusico], [nombre] FROM [Musicos]"></asp:SqlDataSource>
+    <br />
+    <asp:Button ID="btGuardar" runat="server" Text="Añadir Musico al trabajo" ValidationGroup="DatosMusico"/>
+    <asp:Button ID="btCancelar" runat="server" Text="Cancelar" />
+   </asp:Panel>
+        </section>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ProyectoBandadeMusica/contenidos/administrador/MusicoTrabajos.aspx.vb
@@ -0,0 +1,134 @@
+﻿
+Imports System.Data
+Imports System.Data.SqlClient
+
+Partial Class contenidos_administrador_MusicoTrabajos
+    Inherits System.Web.UI.Page
+    Dim cadena As String = "Data Source=(local);Initial Catalog=BandaDeMusica;Integrated Security=SSPI;"
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        pnDatos.Enabled = False
+        listarMusicos()
+        btBorrar.Enabled = False
+    End Sub
+
+    Protected Sub btGuardar_Click(sender As Object, e As EventArgs) Handles btGuardar.Click
+        Dim cnxInsert As New SqlConnection(cadena)
+        Dim sentenciaInsert As String = "insert into MusicoTrabajos(idMusicos,idTrabajo) values(@idMusicos,@idTrabajo)"
+        Dim cmdInsert As New SqlCommand(sentenciaInsert, cnxInsert)
+        cmdInsert.Parameters.AddWithValue("@idMusicos", ddlMusico.SelectedValue)
+        cmdInsert.Parameters.AddWithValue("@idTrabajo", ddltrabajo.SelectedValue)
+
+        Try
+            cnxInsert.Open()
+            If cnxInsert.State = Data.ConnectionState.Open Then
+                cmdInsert.ExecuteNonQuery()
+                cmdInsert.Parameters.Clear()
+                LimpiarCampos()
+            End If
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+        Finally
+            cnxInsert.Close()
+            cnxInsert.Dispose()
+
+            Response.Redirect("~/Contenidos/administrador/MusicoTrabajos.aspx")
+        End Try
+    End Sub
+    Protected Sub LimpiarCampos()
+        ddlMusico.SelectedIndex = -1
+
+        ddltrabajo.SelectedIndex = -1
+    End Sub
+    Public Sub listarMusicos()
+        Dim sentenciaBuscar As String = "select Trabajo.nombre,Trabajo.descripcion,Trabajo.fecha,MusicoTrabajos.idTrabajo,Musicos.nombre,Musicos.apellidos,MusicoTrabajos.idMusicos from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo join Musicos on MusicoTrabajos.idMusicos=Musicos.idMusico"
+        Dim cnxBuscar As New SqlConnection(cadena)
+        Dim cmdBuscar As New SqlCommand(sentenciaBuscar, cnxBuscar)
+        'cmdBuscar.Parameters.AddWithValue("@Cliente", Session("cliente").ToString.Split("#")(0))
+        Dim adaptadorBuscar As New SqlDataAdapter(cmdBuscar)
+        Dim dt As New DataTable
+        adaptadorBuscar.Fill(dt)
+        gvMusicos.DataSource = dt
+        gvMusicos.DataBind()
+    End Sub
+    Protected Sub btCancelar_Click(sender As Object, e As EventArgs) Handles btCancelar.Click
+        LimpiarCampos()
+        btBorrar.Enabled = False
+    End Sub
+
+    Protected Sub gvMusicos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles gvMusicos.SelectedIndexChanged
+        'MsgBox(gvMusicos.SelectedDataKey.Value.ToString)
+        btBorrar.Enabled = True
+
+        Dim idCliente = gvMusicos.SelectedDataKey.Value
+        Dim sentenciaCliente As String = "select Trabajo.nombre,Trabajo.descripcion,Trabajo.fecha,MusicoTrabajos.idTrabajo,Musicos.nombre,Musicos.apellidos,MusicoTrabajos.idMusicos from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo join Musicos on MusicoTrabajos.idMusicos=Musicos.idMusico where idTrabajo=@id"
+        Dim cnxCliente As New SqlConnection(cadena)
+        Dim cmdCliente As New SqlCommand(sentenciaCliente, cnxCliente)
+        cmdCliente.Parameters.AddWithValue("@id", gvMusicos.SelectedDataKey.Value)
+        Dim adaptadorCliente As New SqlDataAdapter(cmdCliente)
+        Dim dt As New DataTable
+        adaptadorCliente.Fill(dt)
+        Dim fila As DataRow = dt.Rows(0)
+
+        ddlMusico.SelectedIndex = -1
+        ddlMusico.SelectedValue = fila("idMusicos")
+
+        ddltrabajo.SelectedIndex = -1
+        ddltrabajo.SelectedValue = fila("idTrabajo")
+
+    End Sub
+
+    Protected Sub btBusqueda_Click(sender As Object, e As EventArgs) Handles btBusqueda.Click
+        Dim sentenciaBuscar As String = "select Trabajo.nombre,Trabajo.descripcion,Trabajo.fecha,MusicoTrabajos.idTrabajo,Musicos.nombre,Musicos.apellidos,MusicoTrabajos.idMusicos from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo join Musicos on MusicoTrabajos.idMusicos=Musicos.idMusico"
+        If Not tbBuscar.Text = Nothing Then
+            If ddlBuscar.SelectedValue = 1 Then
+                sentenciaBuscar = "select Trabajo.nombre,Trabajo.descripcion,Trabajo.fecha,MusicoTrabajos.idTrabajo,Musicos.nombre,Musicos.apellidos,MusicoTrabajos.idMusicos from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo join Musicos on MusicoTrabajos.idMusicos=Musicos.idMusico where Trabajo.nombre like '%" & tbBuscar.Text & "%'"
+            End If
+            If ddlBuscar.SelectedValue = 2 Then
+                sentenciaBuscar = "select Trabajo.nombre,Trabajo.descripcion,Trabajo.fecha,MusicoTrabajos.idTrabajo,Musicos.nombre,Musicos.apellidos,MusicoTrabajos.idMusicos from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo join Musicos on MusicoTrabajos.idMusicos=Musicos.idMusico where Musico.nombre like '%" & tbBuscar.Text & "%'"
+            End If
+            Dim cnxBuscar As New SqlConnection(cadena)
+            Dim cmdBuscar As New SqlCommand(sentenciaBuscar, cnxBuscar)
+            Dim adaptadorBuscar As New SqlDataAdapter(cmdBuscar)
+            Dim dt As New DataTable
+            adaptadorBuscar.Fill(dt)
+            gvMusicos.DataSource = dt
+            gvMusicos.DataBind()
+            LimpiarCampos()
+        End If
+
+    End Sub
+
+    Protected Sub btBorrar_Click(sender As Object, e As EventArgs) Handles btBorrar.Click
+        Dim cnxInsert As New SqlConnection(cadena)
+        Dim sentenciaInsert As String = "delete from MusicoTrabajos where idMusicos=@idMusicos and idTrabajo=@idTrabajo"
+        Dim cmdInsert As New SqlCommand(sentenciaInsert, cnxInsert)
+        MsgBox("Musico:" & gvMusicos.SelectedRow.Cells(7).Text.ToString)
+        cmdInsert.Parameters.AddWithValue("@idMusicos", gvMusicos.SelectedRow.Cells(7).Text.ToString)
+        MsgBox("Trabajo:" & gvMusicos.SelectedDataKey.Value)
+        cmdInsert.Parameters.AddWithValue("@idTrabajos", gvMusicos.SelectedDataKey.Value)
+        Try
+            cnxInsert.Open()
+            If cnxInsert.State = Data.ConnectionState.Open Then
+                cmdInsert.ExecuteNonQuery()
+                cmdInsert.Parameters.Clear()
+
+            End If
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+        Finally
+            cnxInsert.Close()
+            cnxInsert.Dispose()
+            LimpiarCampos()
+            Response.Redirect("~/Contenidos/administrador/MusicoTrabajos.aspx")
+        End Try
+    End Sub
+
+    Protected Sub btCrear_Click(sender As Object, e As EventArgs) Handles btCrear.Click
+        btBorrar.Enabled = False
+        pnDatos.Enabled = True
+        LimpiarCampos()
+    End Sub
+    Protected Sub btMusicoTrabajos_Click(sender As Object, e As EventArgs) Handles btMusicoTrabajos.Click
+        Response.Redirect("~/Contenidos/administrador/Trabajos.aspx")
+    End Sub
+End Class
