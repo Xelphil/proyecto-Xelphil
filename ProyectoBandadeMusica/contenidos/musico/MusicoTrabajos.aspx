03794ce1560b219985d99f17dd75c40fa53a52c0
xelphil <manuelgarin2@hotmail.com>
2018-06-08 11:06:53 +0200

    Añadida la pagina para gestionar para que un musico pueda unirse a un trabajo

---
+++ ProyectoBandadeMusica/contenidos/musico/MusicoTrabajos.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="MusicoTrabajos.aspx.vb" Inherits="contenidos_musico_MusicoTrabajos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <div>
+            <asp:Image ID="imgLogo" runat="server" ImageUrl="~/imagenes/emblemmatic-bandademusica-logo-10.png"/>
+            <asp:Button ID="btCerrarSesion" runat="server" Text="Cerrar Sesion" />
+        </div> 
+       <nav>
+           <asp:Button ID="btTrabajos" runat="server" Text="Trabajos" />
+           <asp:Button ID="btMusicos" runat="server" Text="Musicos" />
+           <asp:Button ID="btGastos" runat="server" Text="Gastos" />
+       </nav>
+        <section>
+            <h1>Trabajos</h1>
+     <asp:Panel ID="pnBuscador" runat="server">
+            <asp:Panel ID="pnBuscar" runat="server">
+                <asp:Label ID="lbBuscar" runat="server" Text="Buscar por: "></asp:Label>
+                <asp:DropDownList ID="ddlBuscar" runat="server">
+                    <asp:ListItem Text="Nombre" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="Ciudad" Value="2"></asp:ListItem>
+                    <asp:ListItem Text="Completo" Value="3"></asp:ListItem>
+                </asp:DropDownList>
+                <asp:TextBox ID="tbBuscar" runat="server"></asp:TextBox>
+                <asp:Button ID="btBusqueda" runat="server" Text="Buscar..." />
+            </asp:Panel>
+            <asp:GridView ID="gvMusicos" runat="server" AutoGenerateColumns="False" DataKeyNames="id">
+                <Columns>
+                    <asp:CommandField ShowSelectButton="True" ButtonType="Button" SelectText="Unirse"/>
+                    <asp:BoundField DataField="id" HeaderText="id" ReadOnly="True" InsertVisible="False" SortExpression="id" Visible="false"/>
+                    <asp:BoundField DataField="nombre" HeaderText="Nombre del Trabajo" SortExpression="nombre" />
+                    <asp:BoundField DataField="fecha" HeaderText="Fecha del Trabajo" SortExpression="fecha" />
+                    <asp:BoundField DataField="lugar" HeaderText="Lugar del Trabajo" SortExpression="lugar" />
+                    <asp:BoundField DataField="ciudad" HeaderText="Ciudad donde se realiza el trabajo(Convenio)" SortExpression="ciudad" />
+                    <asp:BoundField DataField="direccion" HeaderText="Direccion del Trabajo" SortExpression="direccion" />
+                    <asp:BoundField DataField="descripcion" HeaderText="Descripcion del Trabajo" SortExpression="descripcion" />
+                    <asp:BoundField DataField="categoriatrabajo" HeaderText="Categoria del Trabajo" SortExpression="categoriatrabajo"/>
+                    <asp:CheckBoxField DataField="completo" HeaderText="¿Esta completo el trabajo?" SortExpression="completo" />
+                </Columns>
+            </asp:GridView>
+        </asp:Panel>
+            <asp:Button ID="Button1" runat="server" Text="Volver a Trabajos" />
+    <br />
+        </section>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ProyectoBandadeMusica/contenidos/musico/MusicoTrabajos.aspx.vb
@@ -0,0 +1,92 @@
+﻿
+Imports System.Data
+Imports System.Data.SqlClient
+
+Partial Class contenidos_musico_MusicoTrabajos
+    Inherits System.Web.UI.Page
+    Dim cadena As String = "Data Source=(local);Initial Catalog=BandaDeMusica;Integrated Security=SSPI;"
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        listarMusicos()
+    End Sub
+
+    Public Sub listarMusicos()
+        ''cambiar a los usuarios
+        Dim sentenciaBuscar As String = "select * from Trabajo where completo=0 and Trabajo.id not in (select Trabajo.id from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo where MusicoTrabajos.idMusicos=@idMusicos)"
+        Dim cnxBuscar As New SqlConnection(cadena)
+        Dim cmdBuscar As New SqlCommand(sentenciaBuscar, cnxBuscar)
+        cmdBuscar.Parameters.AddWithValue("@idMusicos", Session("dUsuario").Rows(0).item(0))
+        Dim adaptadorBuscar As New SqlDataAdapter(cmdBuscar)
+        Dim dt As New DataTable
+        adaptadorBuscar.Fill(dt)
+        gvMusicos.DataSource = dt
+        gvMusicos.DataBind()
+    End Sub
+
+    Protected Sub gvMusicos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles gvMusicos.SelectedIndexChanged
+        'MsgBox(gvMusicos.SelectedDataKey.Value.ToString)
+
+        Dim cnxInsert As New SqlConnection(cadena)
+        Dim sentenciaInsert As String = "insert into MusicoTrabajos(idMusicos,idTrabajo) values(@idMusicos,@idTrabajo)"
+        Dim cmdInsert As New SqlCommand(sentenciaInsert, cnxInsert)
+        cmdInsert.Parameters.AddWithValue("@idMusicos", Session("dUsuario").Rows(0).item(0))
+        cmdInsert.Parameters.AddWithValue("@idTrabajo", gvMusicos.SelectedDataKey.Value)
+        Try
+            cnxInsert.Open()
+            If cnxInsert.State = Data.ConnectionState.Open Then
+                cmdInsert.ExecuteNonQuery()
+                cmdInsert.Parameters.Clear()
+            End If
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+        Finally
+            cnxInsert.Close()
+            cnxInsert.Dispose()
+
+            Response.Redirect("~/Contenidos/musico/MusicoTrabajos.aspx")
+        End Try
+
+    End Sub
+
+    Protected Sub btBusqueda_Click(sender As Object, e As EventArgs) Handles btBusqueda.Click
+        Dim sentenciaBuscar As String = "select * from Trabajo where completo=0 and Trabajo.id not in (select Trabajo.id from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo where MusicoTrabajos.idMusicos=@idMusicos)"
+        If Not tbBuscar.Text = Nothing Then
+            If ddlBuscar.SelectedValue = 1 Then
+                sentenciaBuscar = "select * from Trabajo where completo=0 and Trabajo.id not in (select Trabajo.id from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo where MusicoTrabajos.idMusicos=@idMusicos) and nombre like '%" & tbBuscar.Text & "%'"
+            End If
+            If ddlBuscar.SelectedValue = 2 Then
+                sentenciaBuscar = "select * from Trabajo where completo=0 and Trabajo.id not in (select Trabajo.id from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo where MusicoTrabajos.idMusicos=@idMusicos) and ciudad like '%" & tbBuscar.Text & "%'"
+            End If
+            If ddlBuscar.SelectedValue = 3 Then
+                sentenciaBuscar = "select * from Trabajo where completo=0 and Trabajo.id not in (select Trabajo.id from Trabajo join MusicoTrabajos on Trabajo.id=MusicoTrabajos.idTrabajo where MusicoTrabajos.idMusicos=@idMusicos) and completo = " & tbBuscar.Text & ""
+            End If
+            Dim cnxBuscar As New SqlConnection(cadena)
+            Dim cmdBuscar As New SqlCommand(sentenciaBuscar, cnxBuscar)
+            cmdBuscar.Parameters.AddWithValue("@idMusicos", Session("dUsuario").Rows(0).item(0))
+            Dim adaptadorBuscar As New SqlDataAdapter(cmdBuscar)
+            Dim dt As New DataTable
+            adaptadorBuscar.Fill(dt)
+            gvMusicos.DataSource = dt
+            gvMusicos.DataBind()
+        End If
+
+    End Sub
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Response.Redirect("~/Contenidos/musico/Trabajos.aspx")
+    End Sub
+
+    Protected Sub btMusicos_Click(sender As Object, e As EventArgs) Handles btMusicos.Click
+        Response.Redirect("~/Contenidos/musico/Musicos.aspx")
+    End Sub
+
+    Protected Sub btCerrarSesion_Click(sender As Object, e As EventArgs) Handles btCerrarSesion.Click
+        codigo.salir(Context.User.Identity.Name)
+    End Sub
+
+    Protected Sub btTrabajos_Click(sender As Object, e As EventArgs) Handles btTrabajos.Click
+        Response.Redirect("~/Contenidos/musico/Trabajos.aspx")
+    End Sub
+
+    Protected Sub btGastos_Click(sender As Object, e As EventArgs) Handles btGastos.Click
+        Response.Redirect("~/Contenidos/musico/Gastos.aspx")
+    End Sub
+End Class
