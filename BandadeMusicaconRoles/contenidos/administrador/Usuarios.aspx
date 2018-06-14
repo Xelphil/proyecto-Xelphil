025b83f9b08fce4543d3dab874f76ed805395032
Xelphil <manuelgarin2@hotmail.com>
2018-06-14 20:50:39 +0200

    Implementacion del apartado de Asignación de roles a Usuarios

---
+++ BandadeMusicaconRoles/contenidos/administrador/Usuarios.aspx
@@ -0,0 +1,154 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Usuarios.aspx.vb" Inherits="contenidos_administrador_Usuarios" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        *{
+            font-family: Calibri;
+            margin: 5px;
+        }
+        #cab{
+            border: 2px solid #007ba7;
+            border-top-left-radius: 0.5em;
+            border-top-right-radius: 0.5em;
+            padding: 1em;
+            display:flex;
+            justify-content:space-between;
+        }
+
+        nav{
+            display:flex;
+            padding: 0.2em;
+            justify-content:space-around;
+            background-color:#e8e8e8;
+            align-items:center;
+            align-content:center;
+        }
+        #gvMusicos {
+            margin:auto;
+        }
+
+        #gvMusicos tbody{
+            font-size: 0.9em;
+            text-align: center;
+            margin:auto;
+        }
+        #gvMusicos tbody th{
+            padding: 0.2em;
+            background-color: #909090;
+            color: white;
+        }
+
+        #gvMusicos tbody td{
+            padding: 0.25em;
+            background-color:#e8e8e8;
+        }
+
+        #gvMusicos input{
+            padding: 0.5em;
+            border-radius: 0.5em;
+            transition: background-color 1s,color 1s ease-out;
+            font-weight: bold;
+            margin-right: 0.25em;
+        }
+
+        #gvMusicos input:hover{
+            background-color:#595959;
+            color:white;
+        }
+
+        input[type=submit]{
+            padding: 0.5em;
+            border-radius: 0.5em;
+            transition: background-color 1s,color 1s ease-out;
+            font-weight: bold;
+            margin-right: 0.25em;
+        }
+        input[type=submit]:hover{
+            background-color:#333333;
+            color:white;
+        }
+        section{
+            text-align:center;
+        }
+        #btCrear,#btGuardar{
+            background-color: #009688;
+        }
+        #btCrear:hover,#btGuardar:hover{
+            background-color:#005996;
+            color: white;
+        }
+        #btModificar{
+            background-color: #03a9f4;
+        }
+        #btModificar:hover{
+            background-color:#0227c2;
+            color: white;
+        }
+        #btCancelar,#btBorrar{
+            background-color: #f44336;
+        }
+        #btCancelar:hover,#btBorrar:hover{
+            background-color:#d2190b;
+            color: white;
+        }
+        #pnBuscar{
+            margin-bottom: 10px;
+        }
+        #pnDatos{
+            align-items:center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div id="cab">
+            <asp:Image ID="imgLogo" runat="server" ImageUrl="~/imagenes/emblemmatic-bandademusica-logo-10.png"/>
+            <asp:Button ID="btCerrarSesion" runat="server" Text="Cerrar Sesion" />
+        </div>
+        <nav>
+           <asp:Button ID="btTrabajos" runat="server" Text="Trabajos" />
+           <asp:Button ID="btMusicos" runat="server" Text="Musicos" />
+           <asp:Button ID="btGastos" runat="server" Text="Gastos" />
+           <asp:Button ID="btCatTrab" runat="server" Text="Categoría Trabajos" />
+           <asp:Button ID="btCatMus" runat="server" Text="Categoría Musicos" />
+           <asp:Button ID="btInstrumentos" runat="server" Text="Instrumentos" />
+           <asp:Button ID="btUsuarios" runat="server" Text="Usuarios" />
+       </nav>
+        <section>
+            <h1>Cambiar Roles</h1>
+            <asp:GridView ID="gvMusicos" runat="server" AutoGenerateColumns="False" DataKeyNames="idRol,idMusico">
+                <Columns>
+                    <asp:CommandField ShowSelectButton="True" ButtonType="Button" />
+                    <asp:BoundField DataField="Rol" HeaderText="Rol" SortExpression="Rol"/>
+                    <asp:BoundField DataField="idRol" HeaderText="IdRol" SortExpression="idRol" ReadOnly="True" />
+                    <asp:BoundField DataField="usuario" HeaderText="Usuario" SortExpression="usuario" />
+                    <asp:BoundField DataField="idMusico" HeaderText="IdMusico" SortExpression="idMusico" ReadOnly="True" />
+                </Columns>
+            </asp:GridView>
+            <asp:Button ID="btCrear" runat="server" Text="Añadir rol al músico" />
+        <asp:Button ID="btModificar" runat="server" Text="Modificar rol del músico" />
+        <asp:Button ID="btBorrar" runat="server" Text="Borrar rol al músico" /> 
+            <asp:Panel ID="pnDatos" runat="server">
+
+                <asp:Label ID="lbusuario" runat="server" Text="Usuario: "></asp:Label>
+           <asp:DropDownList ID="ddlusuario" runat="server" DataSourceID="SqlDSusuarios" DataTextField="usuario" DataValueField="idMusico"></asp:DropDownList>
+            <asp:SqlDataSource ID="SqlDSusuarios" runat="server" ConnectionString="<%$ ConnectionStrings:BandaDeMusicaConnectionString %>" SelectCommand="SELECT [idMusico], [usuario] FROM [Musicos]"></asp:SqlDataSource>
+        <br />
+            <asp:Label ID="lbRol" runat="server" Text="Rol: "></asp:Label>
+           <asp:DropDownList ID="ddlrol" runat="server" DataSourceID="SqlDSroles" DataTextField="Rol" DataValueField="idRol"></asp:DropDownList>
+            <asp:SqlDataSource ID="SqlDSroles" runat="server" ConnectionString="<%$ ConnectionStrings:BandaDeMusicaConnectionString %>" SelectCommand="SELECT [idRol], [Rol] FROM [Roles]"></asp:SqlDataSource>
+        <br />
+    <asp:Button ID="btGuardar" runat="server" Text="Guardar" ValidationGroup="DatosMusico"/>
+    <asp:Button ID="btCancelar" runat="server" Text="Cancelar" />
+            </asp:Panel>
+        </section>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ BandadeMusicaconRoles/contenidos/administrador/Usuarios.aspx.vb
@@ -0,0 +1,180 @@
+﻿
+Imports System.Data
+Imports System.Data.SqlClient
+
+Partial Class contenidos_administrador_Usuarios
+    Inherits System.Web.UI.Page
+    Dim cadena As String = "Data Source=(local);Initial Catalog=BandaDeMusica;Integrated Security=SSPI;"
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        pnDatos.Enabled = False
+        listarMusicos()
+        btCrear.Enabled = True
+        btModificar.Enabled = False
+        btBorrar.Enabled = False
+    End Sub
+
+    Protected Sub btGuardar_Click(sender As Object, e As EventArgs) Handles btGuardar.Click
+        MsgBox(Session("modifica").ToString)
+        If Session("modifica").ToString = False Then
+            Dim cnxInsert As New SqlConnection(cadena)
+            Dim sentenciaInsert As String = "insert into Asignacion(idRol,idMusico) values(@idRol,@idMusico)"
+            Dim cmdInsert As New SqlCommand(sentenciaInsert, cnxInsert)
+            cmdInsert.Parameters.AddWithValue("@idRol", ddlrol.SelectedValue)
+            cmdInsert.Parameters.AddWithValue("@idMusico", ddlusuario.SelectedValue)
+            Try
+                cnxInsert.Open()
+                If cnxInsert.State = Data.ConnectionState.Open Then
+                    cmdInsert.ExecuteNonQuery()
+                    cmdInsert.Parameters.Clear()
+                    LimpiarCampos()
+                End If
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            Finally
+                cnxInsert.Close()
+                cnxInsert.Dispose()
+
+                Response.Redirect("~/Contenidos/administrador/Usuarios.aspx")
+            End Try
+        Else
+            MsgBox(gvMusicos.SelectedDataKey.Value.ToString)
+            Dim cnxInsert As New SqlConnection(cadena)
+            Dim sentenciaInsert As String = "update Asignacion set idRol=@idRol,idMusico=@idMusico where idRol=@idRolAntiguo and idMusico=@idMusicoAntiguo"
+            Dim cmdInsert As New SqlCommand(sentenciaInsert, cnxInsert)
+            cmdInsert.Parameters.AddWithValue("@idRol", ddlrol.SelectedValue)
+            cmdInsert.Parameters.AddWithValue("@idMusico", ddlusuario.SelectedValue)
+            cmdInsert.Parameters.AddWithValue("@idRolAntiguo", gvMusicos.SelectedRow.Cells(2).Text)
+            cmdInsert.Parameters.AddWithValue("@idMusicoAntiguo", gvMusicos.SelectedRow.Cells(4).Text)
+            Try
+                cnxInsert.Open()
+                If cnxInsert.State = Data.ConnectionState.Open Then
+                    cmdInsert.ExecuteNonQuery()
+                    cmdInsert.Parameters.Clear()
+
+                End If
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            Finally
+                cnxInsert.Close()
+                cnxInsert.Dispose()
+                Response.Redirect("~/Contenidos/administrador/Usuarios.aspx")
+            End Try
+        End If
+    End Sub
+    Protected Sub LimpiarCampos()
+        ddlrol.SelectedIndex = -1
+
+        ddlusuario.SelectedIndex = -1
+    End Sub
+    Public Sub listarMusicos()
+        Dim sentenciaBuscar As String = "SELECT Roles.Rol, Asignacion.idRol, Musicos.usuario, Asignacion.idMusico FROM Asignacion INNER JOIN Musicos ON Asignacion.idMusico = Musicos.idMusico INNER JOIN Roles ON Asignacion.idRol = Roles.idRol"
+        Dim cnxBuscar As New SqlConnection(cadena)
+        Dim cmdBuscar As New SqlCommand(sentenciaBuscar, cnxBuscar)
+        'cmdBuscar.Parameters.AddWithValue("@Cliente", Session("cliente").ToString.Split("#")(0))
+        Dim adaptadorBuscar As New SqlDataAdapter(cmdBuscar)
+        Dim dt As New DataTable
+        adaptadorBuscar.Fill(dt)
+        gvMusicos.DataSource = dt
+        gvMusicos.DataBind()
+    End Sub
+    Protected Sub btCancelar_Click(sender As Object, e As EventArgs) Handles btCancelar.Click
+        LimpiarCampos()
+        btCrear.Enabled = True
+        btBorrar.Enabled = False
+        btModificar.Enabled = False
+    End Sub
+
+    Protected Sub gvMusicos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles gvMusicos.SelectedIndexChanged
+        'MsgBox(gvMusicos.SelectedDataKey.Value.ToString)
+        btModificar.Enabled = True
+        btBorrar.Enabled = True
+        btCrear.Enabled = False
+
+        Dim idCliente = gvMusicos.SelectedDataKey.Value
+        Dim sentenciaCliente As String = "select * from Asignacion where idRol=@idRol and idMusico=@idMusico"
+        Dim cnxCliente As New SqlConnection(cadena)
+        Dim cmdCliente As New SqlCommand(sentenciaCliente, cnxCliente)
+        cmdCliente.Parameters.AddWithValue("@idRol", gvMusicos.SelectedRow.Cells(2).Text)
+        cmdCliente.Parameters.AddWithValue("@idMusico", gvMusicos.SelectedRow.Cells(4).Text)
+        Dim adaptadorCliente As New SqlDataAdapter(cmdCliente)
+        Dim dt As New DataTable
+        adaptadorCliente.Fill(dt)
+        Dim fila As DataRow = dt.Rows(0)
+
+        ddlrol.SelectedIndex = -1
+        ddlrol.SelectedValue = fila("idRol")
+
+        ddlusuario.SelectedIndex = -1
+        ddlusuario.SelectedValue = fila("idMusico")
+
+    End Sub
+
+    Protected Sub btBorrar_Click(sender As Object, e As EventArgs) Handles btBorrar.Click
+        Dim cnxInsert As New SqlConnection(cadena)
+        Dim sentenciaInsert As String = "delete from Asignacion where idRol=@idRol and idMusico=@idMusico"
+        Dim cmdInsert As New SqlCommand(sentenciaInsert, cnxInsert)
+        cmdInsert.Parameters.AddWithValue("@idRol", gvMusicos.SelectedRow.Cells(2).Text)
+        cmdInsert.Parameters.AddWithValue("@idMusico", gvMusicos.SelectedRow.Cells(4).Text)
+        Try
+            cnxInsert.Open()
+            If cnxInsert.State = Data.ConnectionState.Open Then
+                cmdInsert.ExecuteNonQuery()
+                cmdInsert.Parameters.Clear()
+
+            End If
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+        Finally
+            cnxInsert.Close()
+            cnxInsert.Dispose()
+            LimpiarCampos()
+            Response.Redirect("~/Contenidos/administrador/Usuarios.aspx")
+        End Try
+    End Sub
+
+    Protected Sub btModificar_Click(sender As Object, e As EventArgs) Handles btModificar.Click
+        Session("modifica") = True
+        pnDatos.Enabled = True
+        MsgBox(Session("modifica").ToString)
+    End Sub
+
+    Protected Sub btCrear_Click(sender As Object, e As EventArgs) Handles btCrear.Click
+        btModificar.Enabled = False
+        btBorrar.Enabled = False
+        pnDatos.Enabled = True
+        LimpiarCampos()
+        Session("modifica") = False
+    End Sub
+
+    Protected Sub btMusicos_Click(sender As Object, e As EventArgs) Handles btMusicos.Click
+        Response.Redirect("~/Contenidos/administrador/Musicos.aspx")
+    End Sub
+
+    Protected Sub btInstrumentos_Click(sender As Object, e As EventArgs) Handles btInstrumentos.Click
+        Response.Redirect("~/Contenidos/administrador/Instrumentos.aspx")
+    End Sub
+
+    Protected Sub btCatMus_Click(sender As Object, e As EventArgs) Handles btCatMus.Click
+        Response.Redirect("~/Contenidos/administrador/CategoriaMusico.aspx")
+    End Sub
+
+    Protected Sub btCerrarSesion_Click(sender As Object, e As EventArgs) Handles btCerrarSesion.Click
+        codigo.salir(Context.User.Identity.Name)
+    End Sub
+
+    Protected Sub btCatTrab_Click(sender As Object, e As EventArgs) Handles btCatTrab.Click
+        Response.Redirect("~/Contenidos/administrador/CategoriaTrabajo.aspx")
+    End Sub
+
+    Protected Sub btTrabajos_Click(sender As Object, e As EventArgs) Handles btTrabajos.Click
+        Response.Redirect("~/Contenidos/administrador/Trabajos.aspx")
+    End Sub
+
+    Protected Sub btGastos_Click(sender As Object, e As EventArgs) Handles btGastos.Click
+        Response.Redirect("~/Contenidos/administrador/Gastos.aspx")
+    End Sub
+
+    Protected Sub btUsuarios_Click(sender As Object, e As EventArgs) Handles btUsuarios.Click
+        Response.Redirect("~/Contenidos/administrador/Usuarios.aspx")
+    End Sub
+End Class
